a879b43903e79019fc8908c64a6db7a2c6f949a5
Soeren Berg <soeren.berg@protonmail.com>
2022-10-03 14:38:22 +0200

    Outline for parsing expression according to precedence levels.

--- src/Text/Nast/Parser.hs
+++ src/Text/Nast/Parser.hs
@@ -9,6 +9,22 @@ module Text.Nast.Parser (
   , numLiteral
   , signedInt
   , stringLiteral
+  -- * expressions
+  , expression
+  , precedence10
+  , precedence9
+  , precedence8
+  , precedence7
+  , precedence6
+  , precedence5
+  , precedence4
+  , precedence3
+  , precedence2
+  , precedence1
+  , precedence0
+  , primary
+  , identifier
+  , parentheses
   -- * shared
   , eol
   , newline
@@ -89,6 +105,124 @@ stringLiteral = do _ <- char '"'
                    s <- many $ noneOf "\n\""
                    _ <- char '"'
                    annotate $ StringLiteral s
+
+expression :: Parser (Expr Annotation)
+expression = precedence10
+
+{-|
+Precedence level 10 expressions
+
+* Ternary @?~:@ op, ternary infix, right associative;
+
+Here, right associative means that @a ? b : c ? d : e@ is equivalent to
+@a ? b : (c ? d : e)@.
+-}
+precedence10 :: Parser (Expr Annotation)
+precedence10 = precedence9
+
+{-|
+Precedence level 9 expressions
+
+* logical @||@ op, binary infix, left associative; "or" disjunction
+-}
+precedence9 :: Parser (Expr Annotation)
+precedence9 = precedence8
+
+{-|
+Precedence level 8 expressions
+
+* logical @&&@ op, binary infix, left associative; "and" conjunction
+-}
+precedence8 :: Parser (Expr Annotation)
+precedence8 = precedence7
+
+{-|
+Precedence level 7 expressions
+
+* logical @==@ op, binary infix, left associative; equal to
+* logical @!=@ op, binary infix, left associative; not equal to
+-}
+precedence7 :: Parser (Expr Annotation)
+precedence7 = precedence6
+
+{-|
+Precedence level 6 expressions
+
+* @<@  op, binary infix, left associative; smaller than
+* @<=@ op, binary infix, left associative; smaller or equal than
+* @>@  op, binary infix, left associative; greater than
+* @>=@ op, binary infix, left associative; greater or equal than
+-}
+precedence6 :: Parser (Expr Annotation)
+precedence6 = precedence5
+
+{-|
+Precedence level 5 expressions
+
+* @+@ op, binary infix, left associative; addition
+* @-@ op, binary infix, left associative; subtraction
+-}
+precedence5 :: Parser (Expr Annotation)
+precedence5 = precedence4
+
+{-|
+Precedence level 4 expressions
+
+* @*@ op, binary infix, left associative; multiplication
+* @.*@ op, binary infix, left associative; element-wise multiplication
+* @/@ op, binary infix, left associative; division
+* @./@ op, binary infix, left associative; element-wise division
+* @%@ op, binary infix, left associative; modulo
+-}
+precedence4 :: Parser (Expr Annotation)
+precedence4 = precedence3
+
+{-|
+Precedence level 3 expressions
+
+* @\@ op, binary infix, left associative; left division
+* @%\%@ op, binary infix, left associative; integer division
+-}
+precedence3 :: Parser (Expr Annotation)
+precedence3 = precedence2
+
+{-|
+Precedence level 2 expressions
+
+* @!@ op, unary prefix; logical negation
+* @+@ op, unary prefix; promotion (no-op)
+* @-@ op, unary prefix; negation
+-}
+precedence2 :: Parser (Expr Annotation)
+precedence2 = precedence1
+
+{-|
+Precedence level 1 expressions
+
+* @^@ op, binary infix, right associative; exponentiation
+* @.^@ op, binary infix, right associative; element-wise exponentiation
+-}
+precedence1 :: Parser (Expr Annotation)
+precedence1 = precedence0
+
+{-|
+Precedence level 0 expressions
+
+* @'@ op, postfix; matrix transposition
+* @()@ function application
+* @[]@ array & matrix indexing
+-}
+precedence0 :: Parser (Expr Annotation)
+precedence0 = primary
+
+primary :: Parser (Expr Annotation)
+primary = literal <|> parentheses <|> identifier
+
+identifier :: Parser (Expr Annotation)
+identifier = fail "not yet implemented"
+
+parentheses :: Parser (Expr Annotation)
+parentheses = fail "not yet implemented"
 
 eol :: Parser ()
 eol = (lookAhead eof) <|> (void $ char '\n') <?> "end of line"
